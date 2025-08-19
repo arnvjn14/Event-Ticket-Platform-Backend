a8f4e1ed95a774fc7b2dc4c02d180a6f5af2eadb
arnv <arnavj343@gmail.com>
2025-08-20 00:09:20 +0530

    JPA configuration is added

---
+++ src/main/java/com/arnav/event_ticket/config/JpaConfiguration.java
@@ -0,0 +1,10 @@
+package com.arnav.event_ticket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfiguration {
+
+}
