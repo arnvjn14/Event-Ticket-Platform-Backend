f7cf7931b1b861de4151b5db026b2f1b7fe699b9
arnv <arnavj343@gmail.com>
2025-08-19 23:28:07 +0530

    Added UserProvisioning Filter and Security Configuration file, and also the UserRepo to save user in database

---
+++ src/main/java/com/arnav/event_ticket/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.arnav.event_ticket.repositories;
+
+import com.arnav.event_ticket.domain.User;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+}
